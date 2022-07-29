f9e5451f56e712fd41d716a7b1c500ea1e7c1136
Alumnos <Alumnos@LABC2PC18>
2022-07-29 10:56:43 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practica_de_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
